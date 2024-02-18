29919107ece8e886b9d96cb1c8cbc872bca2bd38
Viktor Golev <vwpiligrim@gmail.com>
2024-02-18 23:19:53 +0300

    Added filtering of an array of strings by element length

--- string_array/Program.cs
+++ string_array/Program.cs
@@ -7,5 +7,16 @@ class Program
         Console.WriteLine("Введите элементы массива через запятую и пробел:");
         string input = Console.ReadLine()!;
         string[] inputArray = input.Split(new string[] { ", " }, StringSplitOptions.None);
+        string[] outputArray = new string[inputArray.Length];
+        int outputIndex = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                outputArray[outputIndex] = inputArray[i];
+                outputIndex++;
+            }
+        }
     }
 }
