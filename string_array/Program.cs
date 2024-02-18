e0306fa0308b88a20e81c51f0fe4ac2087d07573
Viktor Golev <vwpiligrim@gmail.com>
2024-02-18 23:06:43 +0300

    User input added

---
+++ string_array/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через запятую и пробел:");
+        string input = Console.ReadLine()!;
+    }
+}
